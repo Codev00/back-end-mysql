2431e6e88b6305c25445fc21e05c6d71077de690
Codev00 <kenngok2@gmail.com>
2023-06-18 09:38:31 +0700

    fix path customer

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/:id", CustomerController.getCustomer);
 router.post("/create", CustomerController.createCustomer);
-router.put("/update/:id", CustomerController.updateCustomer);
+router.put("/edit/:id", CustomerController.updateCustomer);
 router.delete("/delete/:id", CustomerController.deleteCustomer);
 
 export default router;
